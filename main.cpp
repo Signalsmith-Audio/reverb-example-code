1009b57ef0d6b4b21f763308d66ae99c375e90ab
Geraint <luffgd@gmail.com>
2021-11-15 01:54:59 +0000

    Combine diffusion and feedback into a reverb

--- main.cpp
+++ main.cpp
@@ -20,9 +20,10 @@ class ExampleRunner {
 		srand(1);
 		processor.configure(outputWav.sampleRate);
 		
-		outputWav.samples.resize(inputWav.samples.size());
-		for (size_t i = 0; i < inputWav.samples.size(); ++i) {
-			outputWav.samples[i] = processor.process(inputWav.samples[i]);
+		int durationSamples = inputWav.samples.size()/inputWav.channels;
+		outputWav.samples.resize(durationSamples);
+		for (int i = 0; i < durationSamples; ++i) {
+			outputWav.samples[i] = processor.process(inputWav.samples[i*inputWav.channels]);
 		}
 		outputWav.write(outputPrefix + name + ".wav");
 		if (!outputWav.result) {
@@ -31,7 +32,7 @@ class ExampleRunner {
 		}
 	}
 
-	template<int channels, class Processor>
+	template<int channels, bool averageOutputs=true, class Processor>
 	void multiProcessor(std::string name, Processor &&processor) {
 		static_assert(channels%2 == 0, "there must be an even number of channels");
 
@@ -42,26 +43,34 @@ class ExampleRunner {
 		srand(1);
 		processor.configure(outputWav.sampleRate);
 		
-		outputWav.samples.resize(inputWav.samples.size()*2);
-		for (size_t i = 0; i < inputWav.samples.size(); ++i) {
-			double value = inputWav.samples[i];
+		int durationSamples = inputWav.samples.size()/inputWav.channels;
+		outputWav.samples.resize(durationSamples*2);
+		for (int i = 0; i < durationSamples; ++i) {
 
-			// Duplicate mono input into multiple channels
+			// Duplicate input channels as many times as needed
 			std::array<double, channels> array;
-			for (int c = 0; c < channels; ++c) array[c] = value;
+			for (int c = 0; c < channels; ++c) {
+				int inputChannel = c%inputWav.channels;
+				array[c] = inputWav.samples[i*inputWav.channels + inputChannel];
+			}
 			
 			array = processor.process(array);
 			
-			// Mix down into stereo for example output
-			double left = 0, right = 0;
-			for (int c = 0; c < channels; c += 2) {
-				left += array[c];
-				right += array[c + 1];
+			if (averageOutputs) {
+				// Mix down into stereo for example output
+				double left = 0, right = 0;
+				for (int c = 0; c < channels; c += 2) {
+					left += array[c];
+					right += array[c + 1];
+				}
+				
+				constexpr double scaling = 2.0/channels;
+				outputWav.samples[2*i] = left*scaling;
+				outputWav.samples[2*i + 1] = right*scaling;
+			} else {
+				outputWav.samples[2*i] = array[0];
+				outputWav.samples[2*i + 1] = array[1];
 			}
-			
-			constexpr double scaling = 2.0/channels;
-			outputWav.samples[2*i] = left*scaling;
-			outputWav.samples[2*i + 1] = right*scaling;
 		}
 		outputWav.write(outputPrefix + name + ".wav");
 		if (!outputWav.result) {
@@ -69,31 +78,36 @@ class ExampleRunner {
 			std::exit(1);
 		}
 	}
+	
+	void extendDuration(double seconds) {
+		int extraSamples = seconds*inputWav.sampleRate;
+		inputWav.samples.resize(inputWav.samples.size() + extraSamples, 0);
+	}
 public:
-	ExampleRunner(std::string inputFile, std::string outputPrefix, double durationSeconds=0) : inputWav(inputFile), outputPrefix(outputPrefix) {
+	ExampleRunner(std::string inputFile, std::string outputPrefix) : inputWav(inputFile), outputPrefix(outputPrefix) {
 		if (!inputWav.result) {
 			std::cerr << inputWav.result.reason << "\n";
 			std::exit(1);
 		}
-		if (inputWav.channels != 1) {
-			std::cerr << "Input WAV must be mono.\n";
-			std::exit(1);
-		}
-		
-		// Pad if needed
-		int durationSamples = durationSeconds*inputWav.sampleRate;
-		if ((int)inputWav.samples.size() < durationSamples) {
-			inputWav.samples.resize(durationSamples, 0);
-		}
 	}
 
 	void runExamples() {
-		monoProcessor("passthrough", Passthrough());
+		extendDuration(3);
 		monoProcessor("single-channel-feedback", SingleChannelFeedback());
 		multiProcessor<4>("multi-channel-feedback-4", MultiChannelFeedback<4>());
 		multiProcessor<8>("multi-channel-feedback-8", MultiChannelFeedback<8>());
 		multiProcessor<4>("multi-channel-feedback-householder-4", MultiChannelMixedFeedback<4>());
 		multiProcessor<8>("multi-channel-feedback-householder-8", MultiChannelMixedFeedback<8>());
+		multiProcessor<4, false>("diffuser-equal-4-3", DiffuserHalfLengths<4, 3>(200));
+		multiProcessor<8, false>("diffuser-equal-8-4", DiffuserHalfLengths<8, 4>(200));
+		multiProcessor<8, false>("diffuser-equal-8-6-long", DiffuserEqualLengths<8, 6>(3000));
+		multiProcessor<8, false>("diffuser-halves-8-6-long", DiffuserHalfLengths<8, 6>(3000));
+		extendDuration(1);
+		multiProcessor<8, true>("reverb-basic-8-short", BasicReverb<8, 4>(50, 2.5));
+		extendDuration(2);
+		multiProcessor<8, true>("reverb-basic-8-mix", BasicReverb<8, 4>(100, 3, 1, 0.25));
+		extendDuration(2);
+		multiProcessor<8, true>("reverb-basic-8-long", BasicReverb<8, 4>(100, 10));
 	}
 };
 
@@ -102,6 +116,6 @@ int main(int argc, const char **argv) {
 		std::cerr << "Missing: input.wav output-prefix-\n";
 		return 1;
 	}
-	ExampleRunner runner(argv[1], argv[2], 3);
+	ExampleRunner runner(argv[1], argv[2]);
 	runner.runExamples();
 }
--- reverb-example-code.h
+++ reverb-example-code.h
@@ -10,14 +10,6 @@ double randomInRange(double low, double high) {
 	return low + unitRand*(high - low);
 }
 
-struct Passthrough {
-	void configure(double) {}
-	
-	double process(double sample) {
-		return sample;
-	}
-};
-
 // Despite the mess, this is a simple delay class which rounds to the nearest sample.
 using Delay = signalsmith::delay::Delay<double, signalsmith::delay::InterpolatorNearest>;
 
@@ -49,17 +41,17 @@ template<int channels=8>
 struct MultiChannelFeedback {
 	using Array = std::array<double, channels>;
 
-	double delayMsLow = 50, delayMsHigh = 150;
+	double delayMs = 150;
 	double decayGain = 0.85;
 
 	std::array<int, channels> delaySamples;
 	std::array<Delay, channels> delays;
 	
 	void configure(double sampleRate) {
-		double delaySamplesLow = delayMsLow*0.001*sampleRate;
-		double delaySamplesHigh = delayMsHigh*0.001*sampleRate;
+		double delaySamplesBase = delayMs*0.001*sampleRate;
 		for (int c = 0; c < channels; ++c) {
-			delaySamples[c] = randomInRange(delaySamplesLow, delaySamplesHigh);
+			double r = c*1.0/channels;
+			delaySamples[c] = std::pow(2, r)*delaySamplesBase;
 			delays[c].resize(delaySamples[c] + 1);
 			delays[c].reset();
 		}
@@ -83,17 +75,17 @@ struct MultiChannelFeedback {
 template<int channels=8>
 struct MultiChannelMixedFeedback {
 	using Array = std::array<double, channels>;
-	double delayMsLow = 100, delayMsHigh = 200;
+	double delayMs = 150;
 	double decayGain = 0.85;
 
 	std::array<int, channels> delaySamples;
 	std::array<Delay, channels> delays;
 	
 	void configure(double sampleRate) {
-		double delaySamplesLow = delayMsLow*0.001*sampleRate;
-		double delaySamplesHigh = delayMsHigh*0.001*sampleRate;
+		double delaySamplesBase = delayMs*0.001*sampleRate;
 		for (int c = 0; c < channels; ++c) {
-			delaySamples[c] = randomInRange(delaySamplesLow, delaySamplesHigh);
+			double r = c*1.0/channels;
+			delaySamples[c] = std::pow(2, r)*delaySamplesBase;
 			delays[c].resize(delaySamples[c] + 1);
 			delays[c].reset();
 		}
@@ -123,8 +115,6 @@ struct DiffusionStep {
 	using Array = std::array<double, channels>;
 	double delayMsRange = 50;
 	
-	DiffusionStep(double delayMsRange) : delayMsRange(delayMsRange) {}
-
 	std::array<int, channels> delaySamples;
 	std::array<Delay, channels> delays;
 	std::array<bool, channels> flipPolarity;
@@ -132,7 +122,9 @@ struct DiffusionStep {
 	void configure(double sampleRate) {
 		double delaySamplesRange = delayMsRange*0.001*sampleRate;
 		for (int c = 0; c < channels; ++c) {
-			delaySamples[c] = randomInRange(0, delaySamplesRange);
+			double rangeLow = delaySamplesRange*c/channels;
+			double rangeHigh = delaySamplesRange*(c + 1)/channels;
+			delaySamples[c] = randomInRange(rangeLow, rangeHigh);
 			delays[c].resize(delaySamples[c] + 1);
 			delays[c].reset();
 			flipPolarity[c] = rand()%2;
@@ -157,5 +149,93 @@ struct DiffusionStep {
 		}
 
 		return mixed;
+	}
+};
+
+template<int channels=8, int stepCount=4>
+struct DiffuserEqualLengths {
+	using Array = std::array<double, channels>;
+
+	using Step = DiffusionStep<channels>;
+	std::array<Step, stepCount> steps;
+
+	DiffuserEqualLengths(double totalDiffusionMs) {
+		for (auto &step : steps) {
+			step.delayMsRange = totalDiffusionMs/stepCount;
+		}
+	}
+	
+	void configure(double sampleRate) {
+		for (auto &step : steps) step.configure(sampleRate);
+	}
+	
+	Array process(Array samples) {
+		for (auto &step : steps) {
+			samples = step.process(samples);
+		}
+		return samples;
+	}
+};
+
+template<int channels=8, int stepCount=4>
+struct DiffuserHalfLengths {
+	using Array = std::array<double, channels>;
+
+	using Step = DiffusionStep<channels>;
+	std::array<Step, stepCount> steps;
+
+	DiffuserHalfLengths(double diffusionMs) {
+		for (auto &step : steps) {
+			diffusionMs *= 0.5;
+			step.delayMsRange = diffusionMs;
+		}
+	}
+	
+	void configure(double sampleRate) {
+		for (auto &step : steps) step.configure(sampleRate);
+	}
+	
+	Array process(Array samples) {
+		for (auto &step : steps) {
+			samples = step.process(samples);
+		}
+		return samples;
+	}
+};
+
+template<int channels=8, int diffusionSteps=4>
+struct BasicReverb {
+	using Array = std::array<double, channels>;
+	
+	MultiChannelMixedFeedback<channels> feedback;
+	DiffuserHalfLengths<channels, diffusionSteps> diffuser;
+	double dry, wet;
+
+	BasicReverb(double roomSizeMs, double rt60, double dry=0, double wet=1) : diffuser(roomSizeMs), dry(dry), wet(wet) {
+		feedback.delayMs = roomSizeMs;
+
+		// How long does our signal take to go around the feedback loop?
+		double typicalLoopMs = roomSizeMs*1.5;
+		// How many times will it do that during our RT60 period?
+		double loopsPerRt60 = rt60/(typicalLoopMs*0.001);
+		// This tells us how many dB to reduce per loop
+		double dbPerCycle = -60/loopsPerRt60;
+
+		feedback.decayGain = std::pow(10, dbPerCycle*0.05);
+	}
+	
+	void configure(double sampleRate) {
+		feedback.configure(sampleRate);
+		diffuser.configure(sampleRate);
+	}
+	
+	Array process(Array input) {
+		Array diffuse = diffuser.process(input);
+		Array longLasting = feedback.process(diffuse);
+		Array output;
+		for (int c = 0; c < channels; ++c) {
+			output[c] = dry*input[c] + wet*longLasting[c];
+		}
+		return output;
 	}
 };
